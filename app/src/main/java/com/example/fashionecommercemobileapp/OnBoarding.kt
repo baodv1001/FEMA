418adcbed62508497664136fa2e137311238ae5b
Trung Huỳnh <57293778+trunghuynh2304@users.noreply.github.com>
2021-04-27 00:11:30 +0700

    Add activity
    Splash, On Boarding, Login, Sign Up

---
+++ app/src/main/java/com/example/fashionecommercemobileapp/OnBoarding.kt
@@ -0,0 +1,11 @@
+package com.example.fashionecommercemobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
