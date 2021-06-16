6dfec42e52d17461cb276a1a66d0079de398098f
Trương Thành Quang <70526874+Quangaka@users.noreply.github.com>
2021-06-17 00:43:58 +0700

    upload image
    Fix dbConnect

--- AccessData/FEMA/Image/upload_image.php
+++ AccessData/FEMA/Image/upload_image.php
@@ -4,11 +4,12 @@
         $db = new dbConnect();
 
         $uploadpath = 'http://laptop-0qnm76ck/fashionecommerceapp/Image/';
+        $file_path = $uploadpath.basename( $_FILES['uploaded_file']['name']);
+        
         if(!is_dir($file_path)){
             //Directory does not exist, so lets create it.
             mkdir($file_path, 0755, true);
         }
-        $file_path = $uploadpath.basename( $_FILES['uploaded_file']['name']);
         if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
             echo "success";
         } else{
